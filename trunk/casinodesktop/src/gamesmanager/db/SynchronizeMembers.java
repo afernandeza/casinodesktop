03d215bb3e2f18351e9ebf4ca2ac8559b5e76ca2
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-03-01 07:28:07 +0000

    progress bar modified

---
+++ trunk/casinodesktop/src/gamesmanager/db/SynchronizeMembers.java
@@ -0,0 +1,53 @@
+package gamesmanager.db;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Random;
+
+import javax.swing.JProgressBar;
+import javax.swing.SwingWorker;
+
+public class SynchronizeMembers extends SwingWorker<Void, Void> implements PropertyChangeListener{
+
+	private JProgressBar pb;
+	
+	public SynchronizeMembers(JProgressBar pb){
+		this.pb = pb;
+		this.addPropertyChangeListener(this);
+	}
+	
+	@Override
+	protected Void doInBackground() throws Exception {
+		Random random = new Random();
+        int progress = 0;
+        setProgress(0);
+        try {
+            Thread.sleep(1000 + random.nextInt(2000));
+        } catch (InterruptedException ignore) {
+        	
+        }
+        while (progress < 100) {
+            try {
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException ignore) {}
+            progress += random.nextInt(10);
+            setProgress(Math.min(progress, 100));
+        }
+        return null;
+	}
+	
+	@Override
+	protected void done(){
+		this.pb.setVisible(false);
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		if(evt.getPropertyName().equals("progress")){
+            int progress = (Integer) evt.getNewValue();
+            this.pb.setIndeterminate(false);
+            this.pb.setValue(progress);
+		}
+	}
+	
+}
--- trunk/casinodesktop/src/gamesmanager/ui/forms/CheckMemberForm.java
+++ trunk/casinodesktop/src/gamesmanager/ui/forms/CheckMemberForm.java
@@ -1,5 +1,6 @@
 package gamesmanager.ui.forms;
 
+import gamesmanager.db.SynchronizeMembers;
 import gamesmanager.ui.Helpers;
 
 import java.awt.GridBagConstraints;
@@ -9,16 +10,18 @@ import java.awt.event.KeyListener;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 
 public class CheckMemberForm extends JPanel implements KeyListener{
 
 	private static final long serialVersionUID = 1L;
 	private JTextField memberid;
+	private JProgressBar syncpb;
+	private GridBagConstraints c = new GridBagConstraints();
 	
 	public CheckMemberForm(){
 		super(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
 		
@@ -28,8 +31,26 @@ public class CheckMemberForm extends JPanel implements KeyListener{
 		memberid = new JTextField(20);
 		this.add(memberid, c);
 		
+		syncpb = new JProgressBar(0, 100);
+		syncpb.setValue(0);
+		syncpb.setStringPainted(true);
+		c.gridy = 1;
+		c.gridx = 0;
+		c.gridwidth = 2;
+		
+		this.add(new JLabel(" "), c);
+		c.gridy = 2;
+		this.add(syncpb, c);	
+		syncpb.setVisible(false);
+		
 		this.setBackground(Helpers.LIGHTBLUE);
 		this.memberid.addKeyListener(this);
+	}
+	
+	public void startSync(){
+		syncpb.setVisible(true);
+		SynchronizeMembers sm = new SynchronizeMembers(this.syncpb);
+		sm.execute();
 	}
 	
 	public void setFocus(){

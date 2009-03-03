2d6b39157959a4ae5f6c179285a3c7774882840d
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-03-03 18:27:05 +0000

    Conexion a base de datos.

--- trunk/casinodesktop/src/gamesmanager/db/DatabaseOperations.java
+++ trunk/casinodesktop/src/gamesmanager/db/DatabaseOperations.java
@@ -4,14 +4,24 @@ import gamesmanager.beans.Member;
 
 public class DatabaseOperations {
 
-	public static boolean login(String username, String password){
+	public static boolean login(String username, String password) {
 		boolean authorized = true;
 		System.out.println("authorizing");
 		return authorized;
 	}
-	
-	public static Member findMember(String memberid){
+
+	public static Member findMember(String memberid) {
 		return null;
 	}
-	
+
+	public static boolean connect() {
+		boolean connected = false;
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException cnfe) {
+			System.err.println("Couldn't find driver class:");
+			cnfe.printStackTrace();
+		}
+		return connected;
+	}
 }

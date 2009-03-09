dac8c9e9a9cc60c090a9d2f6edd9f24a72c2fa42
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-03-09 07:00:20 +0000

    delete stored procedures

--- trunk/casinodesktop/src/gamesmanager/deliverables/queries/insertclientcall.sql
+++ trunk/casinodesktop/src/gamesmanager/deliverables/queries/insertclientcall.sql
@@ -1,1 +1,3 @@
-select insertclient(null, null, null, null, null, null, null, null, null);
+select insertclient(90.34, 'Alfredo', 'Fernandez', 'Acuña', 
+'M', '1986-08-30', '01010101010101001', '5536051150', '0445554534335', 'av club de golf lomas 8',
+'mz 3 lt 7', 'col. greenhouse', 'huixquilucan', '52779', 'Edo Mex', 'Mexico');

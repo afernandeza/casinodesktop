d8a0786f4d1fdc9620cc07c831c8f81d6ff5f4b0
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-03-26 04:02:09 +0000

    authenticate operations

--- casinodesktop/src/gamesmanager/db/DatabaseOperations.java
+++ casinodesktop/src/gamesmanager/db/DatabaseOperations.java
@@ -4,6 +4,8 @@ import gamesmanager.beans.User;
 
 public class DatabaseOperations {
 
+	private final static String AUTH = "? = call authenticate(?, ?)";
+	
 	public static boolean login(User u) {
 		boolean authorized = true;
 		System.out.println("authorizing");

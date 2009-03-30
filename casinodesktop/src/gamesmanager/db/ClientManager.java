904fadc1cc7d20ea823ea3b411e3653957bd69b8
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-03-30 01:57:46 +0000

    find client working now

--- casinodesktop/src/gamesmanager/db/ClientManager.java
+++ casinodesktop/src/gamesmanager/db/ClientManager.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
@@ -17,10 +18,10 @@ public class ClientManager {
 
     private static String INSERT = "{? = call insertclient(?, ?, ?, ?, ?, ?, ?,"
             + "?, ?, ?, ?, ?, ?, ?, ?, ?)}";
-    private static String UPDATE = "{call updateclient[(?, ?, ?, ?, ?, ?, ?,"
-            + "?, ?, ?, ?, ?, ?, ?, ?, ?)]}";
+    private static String UPDATE = "{? = call updateclient(?, ?, ?, ?, ?, ?, ?,"
+            + "?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}";
     private static String DELETE = "{call deleteclient[(?)]}";
-    private static String FIND = "select * from findclient(?)";
+    private static String FIND = "SELECT * FROM findclient(?)";
 
     public static boolean insertClient(Client c) {
         Connection conn = DatabaseManager.connect();
@@ -31,6 +32,7 @@ public class ClientManager {
         try {
             cs = conn.prepareCall(INSERT);
             cs.registerOutParameter(1, Types.BOOLEAN);
+            
             cs.setBigDecimal(2, c.getCredito());
             cs.setString(3, c.getNombres());
             cs.setString(4, c.getAppaterno());
@@ -61,8 +63,62 @@ public class ClientManager {
             cs.execute();
             return cs.getBoolean(1);
         } catch (SQLException e) {
-            // e.printStackTrace();
-            System.out.println("Error inserting client: " + e.getMessage());
+            if(Helpers.DEBUG){
+                // e.printStackTrace();
+                System.out.println("Error inserting client: " + e.getMessage());
+            }
+        } finally {
+            DatabaseManager.close(cs);
+            DatabaseManager.close(conn);
+        }
+        return false;
+    }
+    
+    public static boolean updateClient(Client c) {
+        Connection conn = DatabaseManager.connect();
+        CallableStatement cs = null;
+        if (conn == null) {
+            return false;
+        }
+        try {
+            cs = conn.prepareCall(UPDATE);
+            cs.registerOutParameter(1, Types.BOOLEAN);
+            
+            cs.setString(2, c.getId());
+            cs.setBigDecimal(3, c.getCredito());
+            cs.setString(4, c.getNombres());
+            cs.setString(5, c.getAppaterno());
+            cs.setString(6, c.getApmaterno());
+            cs.setString(7, c.getSexo() + "");
+            cs.setDate(8, new Date(c.getFechanac().getTime()));
+
+            File foto = c.getFoto();
+            InputStream is = c.getFotoInputStream();
+            if (is != null) {
+                cs.setBinaryStream(9, is, (int) foto.length());
+            } else {
+                throw new NullPointerException("Foto es null");
+            }
+
+            cs.setString(10, c.getTelcasa());
+            cs.setString(11, c.getTelcel());
+
+            Address d = c.getAddress();
+            cs.setString(12, d.getCallenum());
+            cs.setString(13, d.getNumint());
+            cs.setString(14, d.getColonia());
+            cs.setString(15, d.getMunicipio());
+            cs.setString(16, d.getCodigopostal());
+            cs.setString(17, d.getEstado());
+            cs.setString(18, d.getPais());
+
+            cs.execute();
+            return cs.getBoolean(1);
+        } catch (SQLException e) {
+            if(Helpers.DEBUG){
+                // e.printStackTrace();
+                System.out.println("Error updating client: " + e.getMessage());
+            }
         } finally {
             DatabaseManager.close(cs);
             DatabaseManager.close(conn);
@@ -72,57 +128,44 @@ public class ClientManager {
 
     public static Client findClient(String id) {
         Connection conn = DatabaseManager.connect();
-        CallableStatement cs = null;
+        PreparedStatement pstmt = null;
         if (conn == null) {
             return null;
         }
         Client c = null;
+        ResultSet rs = null;
         try {
-            cs = conn.prepareCall(FIND);
-            cs.setString(1, id);
+            pstmt = conn.prepareStatement(FIND);
+            pstmt.setString(1, id);
 
-            cs.registerOutParameter(1, Types.VARCHAR);
-            cs.registerOutParameter(2, Types.VARCHAR);
-            cs.registerOutParameter(3, Types.VARCHAR);
-            cs.registerOutParameter(4, Types.CHAR);
-            cs.registerOutParameter(5, Types.DATE);
-            cs.registerOutParameter(6, Types.BLOB);
-            cs.registerOutParameter(7, Types.VARCHAR);
-            cs.registerOutParameter(8, Types.VARCHAR);
-            cs.registerOutParameter(9, Types.VARCHAR);
-            cs.registerOutParameter(10, Types.VARCHAR);
-            cs.registerOutParameter(11, Types.VARCHAR);
-            cs.registerOutParameter(12, Types.VARCHAR);
-            cs.registerOutParameter(13, Types.VARCHAR);
-            cs.registerOutParameter(14, Types.VARCHAR);
-            cs.registerOutParameter(15, Types.VARCHAR);
-            cs.registerOutParameter(16, Types.VARCHAR);
-            cs.registerOutParameter(17, Types.VARCHAR);
-
-            ResultSet rs = cs.executeQuery();
+            rs = pstmt.executeQuery();
             rs.next();
             c = new Client();
-            c.setNombres(cs.getString(1));
-            c.setAppaterno(cs.getString(2));
-            c.setApmaterno(cs.getString(3));
-            c.setSexo(cs.getString(4).charAt(0));
-            c.setFechanac(cs.getDate(5));
-            // c.setFoto(cs.getBlob(6));
-            c.setTelcasa(cs.getString(7));
-            c.setTelcel(cs.getString(8));
-
-            System.out.println(c);
+            c.setId(rs.getString(1));
+            c.setNombres(rs.getString(2));
+            c.setAppaterno(rs.getString(3));
+            c.setApmaterno(rs.getString(4));
+            c.setSexo(rs.getString(5));
+            c.setFechanac(rs.getDate(6));
+            c.setFotoImage(rs.getBytes(7));
+            c.setTelcasa(rs.getString(8));
+            c.setTelcel(rs.getString(9));
+            c.setMembersince(rs.getString(10));
 
             Address a = new Address();
-            a.setCallenum(cs.getString(9));
-            a.setNumint(cs.getString(10));
-            a.setColonia(cs.getString(11));
-            a.setMunicipio(cs.getString(12));
-            a.setCodigopostal(cs.getString(13));
-            a.setEstado(cs.getString(14));
-            a.setPais(cs.getString(15));
+            a.setAddressid(rs.getInt(11));
+            a.setCallenum(rs.getString(12));
+            a.setNumint(rs.getString(13));
+            a.setColonia(rs.getString(14));
+            a.setMunicipio(rs.getString(15));
+            a.setCodigopostal(rs.getString(16));
+            a.setEstado(rs.getString(17));
+            a.setPais(rs.getString(18));
+            c.setAddress(a);
+            
+            c.setCredito(rs.getBigDecimal(19));
 
-            System.out.println(a);
+            System.out.println(c);
 
         } catch (SQLException e) {
             if (Helpers.DEBUG) {
@@ -130,7 +173,8 @@ public class ClientManager {
                 System.out.println("Error finding client: " + e.getMessage());
             }
         } finally {
-            DatabaseManager.close(cs);
+            DatabaseManager.close(rs);
+            DatabaseManager.close(pstmt);
             DatabaseManager.close(conn);
         }
         return c;

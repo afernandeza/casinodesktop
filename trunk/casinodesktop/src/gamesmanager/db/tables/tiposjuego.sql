81538f507be6576bb3f64e38ae7fbc844bf43fac
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-03-31 08:25:09 +0000

    tipos de juego table fix

--- trunk/casinodesktop/src/gamesmanager/db/tables/tiposjuego.sql
+++ trunk/casinodesktop/src/gamesmanager/db/tables/tiposjuego.sql
@@ -6,7 +6,8 @@ CREATE TABLE tiposjuego
 (
   tipojuegoid integer NOT NULL,
   tipo character varying(100) NOT NULL,
-  CONSTRAINT tiposjuego_pkey PRIMARY KEY (tipojuegoid)
+  CONSTRAINT tiposjuego_pkey PRIMARY KEY (tipojuegoid),
+  CONSTRAINT tiposjuego_tipo_key UNIQUE (tipo)
 )
 WITH (OIDS=FALSE);
 ALTER TABLE tiposjuego OWNER TO casindesktopapp;

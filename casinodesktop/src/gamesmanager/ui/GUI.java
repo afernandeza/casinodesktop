93e21f24d85475f3605056ef27a80e8766d15bbf
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-04-07 20:17:51 +0000

    transparent jpanels

--- casinodesktop/src/gamesmanager/ui/GUI.java
+++ casinodesktop/src/gamesmanager/ui/GUI.java
@@ -26,8 +26,6 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class GUI extends JFrame implements ActionListener {
 

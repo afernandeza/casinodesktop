b21b0ca01ea57fc0ff8c0c2227a147b89d88ba29
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-04-07 20:46:23 +0000

    corrected dumps

--- trunk/casinodesktop/src/gamesmanager/db/dumps/pgdumpcommand.sql
+++ trunk/casinodesktop/src/gamesmanager/db/dumps/pgdumpcommand.sql
@@ -1,3 +1,6 @@
-/Applications/pgAdmin3.app/Contents/SharedSupport/pg_dump -i -d -U casindesktopapp casinolocal > dumpfile.sql
 
-casindesktopapp
+with data
+/Applications/pgAdmin3.app/Contents/SharedSupport/pg_dump -i -h localhost -p 5432 -U postgres -F c -b -D -v -f "/Users/afa/Desktop/test.backup" casinolocal
+
+without data
+/Applications/pgAdmin3.app/Contents/SharedSupport/pg_dump -i -h localhost -p 5432 -U postgres -F c -b -v -f "/Users/afa/Desktop/noinserts.backup" casinolocal

3b5c48305190c48166820f834e336cbcf2a7d246
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-04-07 16:51:18 +0000

    icons and user manual

--- trunk/casinodesktop/src/gamesmanager/ui/GuiDialogs.java
+++ trunk/casinodesktop/src/gamesmanager/ui/GuiDialogs.java
@@ -2,27 +2,36 @@ package gamesmanager.ui;
 
 import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class GuiDialogs {
 
+    private static final ImageIcon INFO = Helpers.createImage(
+            "images/infoicon.png", GuiDialogs.class);
+    private static final ImageIcon QUESTION = Helpers.createImage(
+            "images/questionicon.png", GuiDialogs.class);
+    private static final ImageIcon ERROR = Helpers.createImage(
+            "images/erroricon.png", GuiDialogs.class);
+    private static final ImageIcon WARNING = Helpers.createImage(
+            "images/warningicon.png", GuiDialogs.class);
     private static final String PERM_ERROR = "Usted no cuenta con los permisos suficientes "
         + "para realizar la operaci" + Helpers.OACUTE + "n deseada.";
 
     public static void showFormErrorMessage(String msg) {
         JOptionPane.showMessageDialog(null, msg, "Error al introducir datos",
-                JOptionPane.ERROR_MESSAGE);
+                JOptionPane.ERROR_MESSAGE, ERROR);
     }
 
     public static void showErrorMessage(String msg) {
         Toolkit.getDefaultToolkit().beep();
         JOptionPane.showMessageDialog(null, msg, "Error grave",
-                JOptionPane.ERROR_MESSAGE);
+                JOptionPane.ERROR_MESSAGE, ERROR);
     }
 
     public static void showSuccessMessage(String msg) {
         JOptionPane.showMessageDialog(null, msg, "Informaci" + Helpers.OACUTE
-                + "n", JOptionPane.INFORMATION_MESSAGE);
+                + "n", JOptionPane.INFORMATION_MESSAGE, INFO);
     }
 
     public static String showInputDialog(String instructions, Object[] options,
@@ -33,7 +42,7 @@ public class GuiDialogs {
             }
         }
         return (String) JOptionPane.showInputDialog(null, instructions,
-                "Opciones", JOptionPane.QUESTION_MESSAGE, null, options,
+                "Opciones", JOptionPane.QUESTION_MESSAGE, QUESTION, options,
                 options[defindex]);
 
     }
@@ -41,14 +50,14 @@ public class GuiDialogs {
     public static String showInputDialog(String instructions, Object[] options,
             Object selected) {
         return (String) JOptionPane.showInputDialog(null, instructions,
-                "Opciones", JOptionPane.QUESTION_MESSAGE, null, options,
+                "Opciones", JOptionPane.QUESTION_MESSAGE, QUESTION, options,
                 selected);
 
     }
 
     public static int showConfirmDialog(String msg) {
         return JOptionPane.showConfirmDialog(null, msg, "Confirmar",
-                JOptionPane.YES_NO_OPTION);
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, WARNING);
     }
 
     public static void showPermissionsError() {

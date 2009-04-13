c5c0a0295632cd6080b92098ef68d3ec0c64c88b
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-04-13 20:22:45 +0000

    bugs file and database for syncing

--- casinodesktop/src/gamesmanager/db/tables/clientesexternos.sql
+++ casinodesktop/src/gamesmanager/db/tables/clientesexternos.sql
@@ -6,7 +6,18 @@ CREATE TABLE clientesexternos
 (
   casinoid character varying(100) NOT NULL,
   clienteid character varying(100) NOT NULL,
-  CONSTRAINT clientesexternos_pkey PRIMARY KEY (casinoid, clienteid)
+  CONSTRAINT clientesexternos_pkey PRIMARY KEY (casinoid, clienteid),
+  CONSTRAINT clientesexternos_clienteid_key UNIQUE (clienteid)
 )
 WITH (OIDS=FALSE);
 ALTER TABLE clientesexternos OWNER TO casindesktopapp;
+
+-- Index: clientesexternosindex
+
+-- DROP INDEX clientesexternosindex;
+
+CREATE INDEX clientesexternosindex
+  ON clientesexternos
+  USING btree
+  (clienteid);
+

52bab5c28b974010973b864390003bd03127023c
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-04-16 06:22:51 +0000

    sync database fixed

--- casinodesktop/src/gamesmanager/db/tables/sucursales.sql
+++ casinodesktop/src/gamesmanager/db/tables/sucursales.sql
@@ -8,6 +8,7 @@ CREATE TABLE sucursales
   jdbcurl character varying(100) NOT NULL,
   host character varying(100) NOT NULL,
   dbms character varying(100) NOT NULL,
+  latestsyncedqid integer NOT NULL DEFAULT 0,
   CONSTRAINT sucursales_pkey PRIMARY KEY (sucid),
   CONSTRAINT sucursales_jdbcurl_key UNIQUE (jdbcurl)
 )

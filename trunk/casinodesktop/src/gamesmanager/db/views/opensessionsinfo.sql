e7976f1831a263247795559ec5f3e236fc2b882b
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-04-22 02:58:45 +0000

    bug fix with null open sessions

--- trunk/casinodesktop/src/gamesmanager/db/views/opensessionsinfo.sql
+++ trunk/casinodesktop/src/gamesmanager/db/views/opensessionsinfo.sql
@@ -5,7 +5,7 @@
 CREATE OR REPLACE VIEW opensessionsinfo AS 
  SELECT sesiones.sesionid, sesiones.mesaid, tiposjuego.tipo, sesiones.fichasinicio, sesiones.fichasfin, employeessummary.nombre, sesiones.timeopened, sesiones.timeclosed
    FROM sesiones, tiposjuego, employeessummary, mesas
-  WHERE sesiones.mesaid = mesas.mesaid AND mesas.tipojuegoid = tiposjuego.tipojuegoid AND sesiones.empleadoid::text = employeessummary.empleadoid::text AND sesiones.timeclosed = NULL::timestamp with time zone
+  WHERE sesiones.mesaid = mesas.mesaid AND mesas.tipojuegoid = tiposjuego.tipojuegoid AND sesiones.empleadoid::text = employeessummary.empleadoid::text AND sesiones.timeclosed IS NULL
   ORDER BY mesas.mesaid;
 
 ALTER TABLE opensessionsinfo OWNER TO casindesktopapp;

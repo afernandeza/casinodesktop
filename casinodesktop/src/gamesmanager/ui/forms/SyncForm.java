d7ca3e543f1e14fc37e3e007aaa66bc80c339b58
alfredofernandeza <alfredofernandeza@bbc890a8-014d-11de-bcc6-d9108be23e13>
2009-05-17 02:59:14 +0000

    syncing form fixed

--- casinodesktop/src/gamesmanager/ui/forms/SyncForm.java
+++ casinodesktop/src/gamesmanager/ui/forms/SyncForm.java
@@ -1,13 +1,9 @@
 package gamesmanager.ui.forms;
 
-import gamesmanager.beans.Casino;
-import gamesmanager.db.CasinoManager;
 import gamesmanager.db.SyncFormThread;
 import gamesmanager.db.Synchronizer;
 import gamesmanager.db.SynchronizerThread;
-import gamesmanager.ui.GuiDialogs;
 import gamesmanager.ui.Helpers;
-import gamesmanager.ui.session.Session;
 
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -15,21 +11,17 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableColumn;
 
-public class SyncForm extends JFrame implements ActionListener, MouseListener {
+public class SyncForm extends JFrame implements ActionListener {
 
     private static final long serialVersionUID = 1L;
 
@@ -38,46 +30,18 @@ public class SyncForm extends JFrame implements ActionListener, MouseListener {
     private static final int TWIDTH = 500;
     private static final int THEIGHT = 300;
     private GridBagConstraints c = new GridBagConstraints();
-    private JTextField id;
-    private JTextField jdbchost;
-    private JButton addbutton;
     private JButton syncbutton;
     private JButton refreshbutton;
+    private JButton cancel;
     private JTable table;
     private SynInfoTableModel etm;
     private Object[][] syncinfo = null;
-    private Casino cas;
-
-    public String[] OPTIONS = { "Cambiar el URL de JDBC", "Dar casino de baja" };
-    public String INSTRUCTIONS = "Seleccione qu" + Helpers.EACUTE
-            + " desea hacer con el casino seleccionado:";
 
     public SyncForm() {
         super("Sincronizaci" + Helpers.OACUTE + "n");
         this.setLayout(new GridBagLayout());
         c.gridx = 0;
         c.gridy = 0;
-
-        JPanel newcasino = new JPanel();
-        newcasino.setBackground(Helpers.LIGHTBLUE);
-
-        JLabel idlabel = new JLabel("<html><b>ID: </b></html>");
-        newcasino.add(idlabel);
-
-        id = new JTextField(5);
-        newcasino.add(id);
-
-        JLabel jdbchostlabel = new JLabel("<html><b>URL JDBC: </b></html>");
-        newcasino.add(jdbchostlabel);
-
-        jdbchost = new JTextField(18);
-        newcasino.add(jdbchost);
-
-        addbutton = new JButton("Agregar");
-        addbutton.addActionListener(this);
-        newcasino.add(addbutton);
-
-        this.add(newcasino);
 
         JPanel tablepanel = new JPanel();
         tablepanel.setLayout(new GridLayout(1, 0));
@@ -90,7 +54,6 @@ public class SyncForm extends JFrame implements ActionListener, MouseListener {
         this.setColumnWidths();
 
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        table.addMouseListener(this);
         table.setRowSelectionAllowed(true);
 
         table
@@ -120,7 +83,11 @@ public class SyncForm extends JFrame implements ActionListener, MouseListener {
         syncbutton = new JButton("Sincronizar");
         syncbutton.addActionListener(this);
         syncbuttons.add(syncbutton);
-
+        
+        cancel = new JButton("Cancelar");
+        cancel.addActionListener(this);
+        syncbuttons.add(cancel);
+        
         this.add(syncbuttons, c);
 
         this.getContentPane().setBackground(Helpers.LIGHTBLUE);
@@ -143,16 +110,6 @@ public class SyncForm extends JFrame implements ActionListener, MouseListener {
         column.setPreferredWidth(100);
     }
 
-    public boolean validateForm() {
-        String cid = id.getText().trim();
-        String host = jdbchost.getText().trim();
-        if (!cid.equals("") && !host.equals("")) {
-            cas = new Casino(cid, host);
-            return true;
-        }
-        return false;
-    }
-
     public void refresh() {
         SyncFormThread syncer = new SyncFormThread();
         syncer.execute();
@@ -166,20 +123,10 @@ public class SyncForm extends JFrame implements ActionListener, MouseListener {
             SynchronizerThread st = new SynchronizerThread();
             st.execute();
             this.dispose();
-        } else if (src.equals(addbutton)) {
-            if (Session.mayManageCasinos()) {
-                if (this.validateForm()) {
-                    if (CasinoManager.insertCasino(this.cas)) {
-                        GuiDialogs
-                                .showSuccessMessage("El casino ha sido insertado correctamente.");
-                        this.refresh();
-                    }
-                }
-            } else {
-                GuiDialogs.showPermissionsError();
-            }
         } else if (src.equals(refreshbutton)) {
             this.refresh();
+        } else if(src.equals(cancel)){
+            this.dispose();
         }
     }
 
@@ -257,76 +204,5 @@ public class SyncForm extends JFrame implements ActionListener, MouseListener {
                 }
             }
         }
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (e.getClickCount() == 2) {
-            int selindex = table.getSelectedRow();
-            if (selindex != -1) {
-                if (Session.mayManageCasinos()) {
-                    Object o = GuiDialogs.showInputDialog(INSTRUCTIONS,
-                            OPTIONS, 0);
-                    String casinoid = syncinfo[selindex][0].toString();
-                    if (o != null) {
-                        String s = o.toString();
-                        if (s.equals(OPTIONS[0])) {
-                            // update casino
-                            Object obj = GuiDialogs.showInputDialog("Escriba el "
-                                    + "nuevo URL de JDBC:");
-
-                            if (obj != null) {
-                                String newhost = obj.toString();
-                                if(!newhost.equals("")){
-                                    Casino c = new Casino(casinoid, newhost);
-                                    if(CasinoManager.updateCasino(c)){
-                                        GuiDialogs.showSuccessMessage("Casino actualizado correctamente.");
-                                        this.refresh();
-                                    } else {
-                                        GuiDialogs.showErrorMessage("No se pudo actualizar el URL de JDBC");
-                                    }
-                                }
-                            }
-                        } else if (s.equals(OPTIONS[1])) {
-                            //delete casino
-                            int n = GuiDialogs
-                                    .showConfirmDialog(Helpers.OQUESTIONM
-                                            + "Est"
-                                            + Helpers.AACUTE
-                                            + " seguro que desea eliminar el casino"
-                                            + Helpers.CQUESTIONM);
-                            if (n == 0) {
-                                // confirmar
-                                if (CasinoManager.deleteCasino(casinoid)) {
-                                    GuiDialogs.showSuccessMessage("Casino borrado correctamente.");
-                                    this.refresh();
-                                } else {
-                                    //cancelar
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    GuiDialogs.showPermissionsError();
-                }
-            }
-        }
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
     }
 }
